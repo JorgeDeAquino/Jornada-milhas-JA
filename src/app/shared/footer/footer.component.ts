d3890e9d717fcc91555b55b580f4fc15b7910c9c
Jorge Alexandre <jorgeaamsantos@gmail.com>
2024-11-06 16:13:49 -0300

    add componente footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ContainerComponent } from "../container/container.component";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [ ContainerComponent ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
