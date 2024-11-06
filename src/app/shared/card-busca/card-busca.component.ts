cbaac9fd0f2f5d23c9405efd06c4434b8148f06f
Jorge Alexandre <jorgeaamsantos@gmail.com>
2024-11-06 17:10:02 -0300

    add card busca ao home ja funcionando e criado componente de depoimentos

---
+++ src/app/shared/card-busca/card-busca.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+
+@Component({
+  selector: 'app-card-busca',
+  standalone: true,
+  imports: [ MatCardModule ],
+  templateUrl: './card-busca.component.html',
+  styleUrl: './card-busca.component.scss'
+})
+export class CardBuscaComponent {
+
+}
